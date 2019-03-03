73c026dfa771d3850f17e713291590ec43f563b9
lex <lex@debitoor.com>
2019-03-03 13:18:17 +0200

    refactor tests

--- test/integration/GameBoard.test.ts
+++ test/integration/GameBoard.test.ts
@@ -1,4 +1,5 @@
 import {
+  Vector2d,
   Size,
 
   init,
@@ -6,130 +7,137 @@ import {
   moveHoleTo,
   canMoveHoleTo,
   getHoleMoveDirs,
+
 } from '../../src/models/GameBoard';
 
-(<[Size, (size: Size) => number[], any[]][]>[
+(<{
+  size: Size,
+  generator: (size: Size) => number[],
+  expectations: {
+    dir: Vector2d, values: number[],
+  }[],
+}[]>
+  [
+    { // top-left coner
+      size: { width: 2, height: 2 },
+      generator: () => [
+        0, 1,
+        2, 3,
+      ],
+      expectations: [
+        { dir: { x: 1, y: 0 }, values: [1, 0, 2, 3] },
+        { dir: { x: 0, y: 1 }, values: [2, 1, 0, 3] },
+      ],
+    },
 
-  [ // top-left coner
-    { width: 2, height: 2 },
-    () => [
-      0, 1,
-      2, 3,
-    ],
-    [
-      { dir: { x: 1, y: 0 }, values: [1, 0, 2, 3] },
-      { dir: { x: 0, y: 1 }, values: [2, 1, 0, 3] },
-    ],
-  ],
+    { // top-right coner
+      size: { width: 2, height: 2 },
+      generator: () => [
+        1, 0,
+        2, 3,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [0, 1, 2, 3] },
+        { dir: { x: 0, y: 1 }, values: [1, 3, 2, 0] },
+      ],
+    },
 
-  [ // top-right coner
-    { width: 2, height: 2 },
-    () => [
-      1, 0,
-      2, 3,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [0, 1, 2, 3] },
-      { dir: { x: 0, y: 1 }, values: [1, 3, 2, 0] },
-    ],
-  ],
+    { // bottom-right coner
+      size: { width: 2, height: 2 },
+      generator: () => [
+        1, 2,
+        3, 0,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [1, 2, 0, 3] },
+        { dir: { x: 0, y: -1 }, values: [1, 0, 3, 2] },
+      ],
+    },
 
-  [ // bottom-right coner
-    { width: 2, height: 2 },
-    () => [
-      1, 2,
-      3, 0,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [1, 2, 0, 3] },
-      { dir: { x: 0, y: -1 }, values: [1, 0, 3, 2] },
-    ],
-  ],
+    { // bottom-left coner
+      size: { width: 2, height: 2 },
+      generator: () => [
+        1, 2,
+        0, 3,
+      ],
+      expectations: [
+        { dir: { x: 0, y: -1 }, values: [0, 2, 1, 3] },
+        { dir: { x: 1, y: 0 }, values: [1, 2, 3, 0] },
+      ],
+    },
 
-  [ // bottom-left coner
-    { width: 2, height: 2 },
-    () => [
-      1, 2,
-      0, 3,
-    ],
-    [
-      { dir: { x: 0, y: -1 }, values: [0, 2, 1, 3] },
-      { dir: { x: 1, y: 0 }, values: [1, 2, 3, 0] },
-    ],
-  ],
+    { // left
+      size: { width: 3, height: 3 },
+      generator: () => [
+        1, 2, 3,
+        0, 4, 5,
+        6, 7, 8,
+      ],
+      expectations: [
+        { dir: { x: 0, y: -1 }, values: [0, 2, 3, 1, 4, 5, 6, 7, 8] },
+        { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
+        { dir: { x: 0, y: 1 }, values: [1, 2, 3, 6, 4, 5, 0, 7, 8] },
+      ],
+    },
 
-  [ // left
-    { width: 3, height: 3 },
-    () => [
-      1, 2, 3,
-      0, 4, 5,
-      6, 7, 8,
-    ],
-    [
-      { dir: { x: 0, y: -1 }, values: [0, 2, 3, 1, 4, 5, 6, 7, 8] },
-      { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
-      { dir: { x: 0, y: 1 }, values: [1, 2, 3, 6, 4, 5, 0, 7, 8] },
-    ],
-  ],
+    { // top
+      size: { width: 3, height: 3 },
+      generator: () => [
+        1, 0, 3,
+        4, 2, 5,
+        6, 7, 8,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [0, 1, 3, 4, 2, 5, 6, 7, 8] },
+        { dir: { x: 1, y: 0 }, values: [1, 3, 0, 4, 2, 5, 6, 7, 8] },
+        { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
+      ],
+    },
 
-  [ // top
-    { width: 3, height: 3 },
-    () => [
-      1, 0, 3,
-      4, 2, 5,
-      6, 7, 8,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [0, 1, 3, 4, 2, 5, 6, 7, 8] },
-      { dir: { x: 1, y: 0 }, values: [1, 3, 0, 4, 2, 5, 6, 7, 8] },
-      { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
-    ],
-  ],
+    { // right
+      size: { width: 3, height: 3 },
+      generator: () => [
+        1, 2, 3,
+        4, 5, 0,
+        6, 7, 8,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
+        { dir: { x: 0, y: -1 }, values: [1, 2, 0, 4, 5, 3, 6, 7, 8] },
+        { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 5, 8, 6, 7, 0] },
+      ],
+    },
 
-  [ // right
-    { width: 3, height: 3 },
-    () => [
-      1, 2, 3,
-      4, 5, 0,
-      6, 7, 8,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
-      { dir: { x: 0, y: -1 }, values: [1, 2, 0, 4, 5, 3, 6, 7, 8] },
-      { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 5, 8, 6, 7, 0] },
-    ],
-  ],
+    { // bottom
+      size: { width: 3, height: 3 },
+      generator: () => [
+        1, 2, 3,
+        4, 7, 5,
+        6, 0, 8,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [1, 2, 3, 4, 7, 5, 0, 6, 8] },
+        { dir: { x: 0, y: -1 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
+        { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 7, 5, 6, 8, 0] },
+      ],
+    },
 
-  [ // bottom
-    { width: 3, height: 3 },
-    () => [
-      1, 2, 3,
-      4, 7, 5,
-      6, 0, 8,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [1, 2, 3, 4, 7, 5, 0, 6, 8] },
-      { dir: { x: 0, y: -1 }, values: [1, 2, 3, 4, 0, 5, 6, 7, 8] },
-      { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 7, 5, 6, 8, 0] },
-    ],
-  ],
-
-  [ // center
-    { width: 3, height: 3 },
-    () => [
-      1, 2, 3,
-      4, 0, 5,
-      6, 7, 8,
-    ],
-    [
-      { dir: { x: -1, y: 0 }, values: [1, 2, 3, 0, 4, 5, 6, 7, 8] },
-      { dir: { x: 0, y: -1 }, values: [1, 0, 3, 4, 2, 5, 6, 7, 8] },
-      { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 5, 0, 6, 7, 8] },
-      { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 7, 5, 6, 0, 8] },
-    ],
-  ],
-
-]).forEach(([size, generator, expectations]) => {
+    { // center
+      size: { width: 3, height: 3 },
+      generator: () => [
+        1, 2, 3,
+        4, 0, 5,
+        6, 7, 8,
+      ],
+      expectations: [
+        { dir: { x: -1, y: 0 }, values: [1, 2, 3, 0, 4, 5, 6, 7, 8] },
+        { dir: { x: 0, y: -1 }, values: [1, 0, 3, 4, 2, 5, 6, 7, 8] },
+        { dir: { x: 1, y: 0 }, values: [1, 2, 3, 4, 5, 0, 6, 7, 8] },
+        { dir: { x: 0, y: 1 }, values: [1, 2, 3, 4, 7, 5, 6, 0, 8] },
+      ],
+    },
+  ]
+).forEach(({ size, generator, expectations }) => {
 
   describe(`try move hole in [${generator(size)}]`, () => {
     const gameBoard = init({ generator, size });
